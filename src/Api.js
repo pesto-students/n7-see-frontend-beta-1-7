90a6057b477a3c37f8f6cae0d1a98d47e67f49d7
Kiran <Kiran@gitlab.keralapolice.gov.in>
2022-01-05 14:26:33 +0530

    api to glitch

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,4 @@
-export const myApi= 'http://129.154.237.198:3000'
+export const myApi= 'https://lydian-spangle-sphynx.glitch.me'
+// export const myApi= 'http://129.154.237.198:3000'
 // export const myApi= 'https://see-backend.herokuapp.com'
 // export const myApi= 'http://localhost:4000'
